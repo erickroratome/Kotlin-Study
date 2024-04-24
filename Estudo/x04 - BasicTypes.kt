6466596519623159ae26cf009eaed32476bd22a6
Erick Rorato <96999065+erickroratome@users.noreply.github.com>
2024-04-24 09:39:27 -0300

    x04 - BasicTypes.kt
    Tudo sobre types

---
+++ Estudo/x04 - BasicTypes.kt
@@ -0,0 +1,39 @@
+fun main(){
+    //----------------------------------------Basic Types----------------------------------------
+    custom = custom + 3 // custom == 11
+    custom += 7 // custom == 18 | soma
+    custom -= 3 // custom == 15 | subtração
+    custom *= 2 // custom == 30 | multiplicação
+    custom /= 3 // custom == 10 | divisão
+
+
+  
+    /*
+    Integers                    Byte, Short, Int, Long
+    Unsigned integers           UByte, UShort, UInt, ULong
+    Floating-point numbers      Float, Double
+    Booleans                    Boolean
+    Characters                  Char
+    Strings                     String
+     */
+    val d: Int
+    d = 3
+    val e: String = "Hello!"
+    println (d)
+    println (e)
+
+
+    
+    //Exercise
+    /*
+    fun main() {
+    val a: Int      = 1000                    --> Int
+    val b: String   = "log message"           --> String
+    val c: Double   = 3.14                    --> Double
+    val d: Long     = 100_000_000_000_000     --> Long
+    val e: Boolean  = false                   --> Boolean
+    val f: Char     = '\n'                    --> Char
+    }
+    */
+
+}

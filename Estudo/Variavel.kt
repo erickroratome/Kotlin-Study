c2db6af9d332ef06fb80b2cf09f94fa6c59ff96f
Erick Rorato <96999065+erickroratome@users.noreply.github.com>
2024-04-24 09:31:26 -0300

    Create Variavel.kt

---
+++ Estudo/Variavel.kt
@@ -0,0 +1,11 @@
+fun main(){
+  //----------------------------------------Variavel----------------------------------------//
+    // variavel read-only --> val
+    // variavel mutable   --> var
+    val cinco = 5
+    val sete = 7
+    var custom = 10
+
+    custom = 8
+    println(custom)
+}

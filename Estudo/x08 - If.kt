e8413d8469b5a8453576085bd454e731f02e371c
Erick Rorato <96999065+erickroratome@users.noreply.github.com>
2024-04-24 14:30:59 -0300

    Update x08 - If.kt

--- Estudo/x08 - If.kt
+++ Estudo/x08 - If.kt
@@ -1,4 +1,8 @@
 fun main(){
+  
+  // If = se tal coisa for isso faça == 
+  // Else se não for diferente disso faça ===
+  // Criando if comun
   val d: int
   val check = true
 
@@ -8,4 +12,12 @@ fun main(){
     d = 2
   }
   println(d) //1
+
+
+  // Outro if, porém em uma linha
+  val a = 1
+  val b = 2
+  println(if (a > b) a else b) // Retorna o valor: 2
+
+  
 }

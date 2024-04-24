8dc8885d189f1b7957739c19ef4b59de77d25013
Erick Rorato <96999065+erickroratome@users.noreply.github.com>
2024-04-24 10:06:43 -0300

    x05 - Lists.kt

--- Estudo/x05 - Lists.kt
+++ Estudo/x05 - Lists.kt
@@ -8,5 +8,21 @@ fun main(){
     val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle") //Mutable list with explicit type declartion
     println(shapes) // [triangle, square, circle]
 
-    
+    //Ver itens expecificos da lista
+    println("Primeiro item da lista ${readOnleShapes[0]}") //triangle
+
+    // .first() .last() --> primeiro e ultimo item da lista
+    println("Primeiro item da lista é: ${readOnlyShapes.first()}") //triangle
+    println("Primeiro item da lista é: ${readOnlyShapes.last()}")  //circle
+
+    // .count() --> conta o tanto de itens que tem na lista
+    println("Essa lista $[readOnlyShapes.count()] itens")
+
+    // saber se tem tal item na lista
+    println("circle" in readOnlyShapes)
+
+    // .add ou .remove itens na lista
+    readOnlyShapes.add("pentagon")
+    println(shapes) // [triangle, square, circle, pentagon]
+    readOnlyShapes.remove("pentagon") // [triangle, square, circle]
 }

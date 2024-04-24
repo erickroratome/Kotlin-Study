7779beaee566e660d5be475a9028733b02f6f0aa
Erick Rorato <96999065+erickroratome@users.noreply.github.com>
2024-04-24 09:26:17 -0300

    Create print.kt
    Tudo sobre print

---
+++ Estudo1/print.kt
@@ -0,0 +1,5 @@
+fun main(){
+    //----------------------------------------Prints----------------------------------------
+    // print   == printa e não pula linha
+    // println == printa e pula linha
+    println("Hello, World!")
